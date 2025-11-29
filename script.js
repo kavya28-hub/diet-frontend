65b0b47e94116740730fdf0a2479b0d0d4f88332
KAVYA MOOLI <152363530+kavya28-hub@users.noreply.github.com>
2025-11-29 06:41:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.getElementById("dietForm").addEventListener("submit", async function (e
   `;
 
   try {
-    const res = await fetch("https://diet-backend-3.onrender.com/api/recommend", {
+    const res = await fetch("https://diet-backend-4.onrender.com/api/recommend", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body)
@@ -49,6 +49,7 @@ document.getElementById("dietForm").addEventListener("submit", async function (e
     `;
   }
 });
+
 
 
 
